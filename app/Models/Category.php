bfce8c4561892abe53b6be8b9d0c8538cb68324a
Abderrahman Firoud <abder1432@gmail.com>
2023-05-31 17:29:35 +0300

    add  parent and children relationship to category model  and create a factory and a seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -18,4 +18,14 @@ class Category extends Model
         'name',
         'parent_id',
     ];
+
+    public function parent()
+    {
+        return $this->belongsTo($self::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
